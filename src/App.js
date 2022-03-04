546dbcf9114a4ea521e4b30231266a184e1f9046
WTrygar <chandelier.wt@gmail.com>
2022-03-04 10:57:16 +0100

    changes in comments

--- src/App.js
+++ src/App.js
@@ -49,9 +49,6 @@ const addTask = async (task) => {
 
   setTasks([...tasks, data])
 
-  // const id = Math.floor(Math.random() * 10000) + 1
-  // const newTask = { id, ...task }
-  // setTasks([...tasks, newTask])
 }
 
 // Delete Task
